6049c91fe2544229bb5364fcd8d7a945fd81f197
kitewater <97283971+kitewater@users.noreply.github.com>
2024-05-13 12:16:48 +0900

    first commit

---
+++ src/main/java/passionmansour/teambeam/TeambeamApplication.java
@@ -0,0 +1,13 @@
+package passionmansour.teambeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeambeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeambeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/passionmansour/teambeam/TeambeamApplicationTests.java
@@ -0,0 +1,13 @@
+package passionmansour.teambeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeambeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
