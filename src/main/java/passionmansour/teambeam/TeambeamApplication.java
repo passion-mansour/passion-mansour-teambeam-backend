4483d8c798b1c04f0fabf3f5937322d2026b19b3
kitewater <97283971+kitewater@users.noreply.github.com>
2024-05-13 14:56:36 +0900

    기본 세팅
    배포를 위한 기본 환경설정입니다.

--- src/main/java/passionmansour/teambeam/TeambeamApplication.java
+++ src/main/java/passionmansour/teambeam/TeambeamApplication.java
@@ -1,7 +1,10 @@
 package passionmansour.teambeam;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import passionmansour.teambeam.redis.RedisService;
 
 @SpringBootApplication
 public class TeambeamApplication {
@@ -11,4 +14,10 @@ public class TeambeamApplication {
 		System.out.println("hello world");
 	}
 
+	@Bean
+	public CommandLineRunner run(RedisService redisService) {
+		return args -> {
+			redisService.checkRedisConnection();
+		};
+	}
 }
