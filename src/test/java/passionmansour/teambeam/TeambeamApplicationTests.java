2a76061771625c23ff21ce255f0fffa25aa152a0
kitewater <97283971+kitewater@users.noreply.github.com>
2024-05-13 15:03:23 +0900

    fix: test 비활성화
    배포를 위해서 test 비활성화

--- src/test/java/passionmansour/teambeam/TeambeamApplicationTests.java
+++ src/test/java/passionmansour/teambeam/TeambeamApplicationTests.java
@@ -3,10 +3,10 @@ package passionmansour.teambeam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TeambeamApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
