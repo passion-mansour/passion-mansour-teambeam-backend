8e4d1794fb4a11aa5257b0271a839ad0f42c2b28
kitewater <97283971+kitewater@users.noreply.github.com>
2024-05-16 17:37:39 +0900

    fix : 코드 수정
    안 쓰는 import 문 수정
    application-yml 수정

--- src/main/java/passionmansour/teambeam/config/security/SecurityConfig.java
+++ src/main/java/passionmansour/teambeam/config/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import passionmansour.teambeam.service.member.CustomUserDetailsService;
 
 @Configuration
 @RequiredArgsConstructor
