e5064d622baa3c0a70f97930c866096a83a7ccea
toughCircle <toughCircle@gmail.com>
2024-05-17 14:36:03 +0900

    Fix: 개발용 Path 추가

--- src/main/java/passionmansour/teambeam/config/security/SecurityConfig.java
+++ src/main/java/passionmansour/teambeam/config/security/SecurityConfig.java
@@ -44,7 +44,6 @@ public class SecurityConfig {
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
 
