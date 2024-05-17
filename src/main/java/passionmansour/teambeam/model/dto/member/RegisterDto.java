ceb3574dee81a547850b39dcd7c5bc258f8e484c
toughCircle <150303834+toughCircle@users.noreply.github.com>
2024-05-17 12:57:33 +0900

    Revert "Feat: Implementing Member Functions"

--- src/main/java/passionmansour/teambeam/model/dto/member/RegisterDto.java
+++ src/main/java/passionmansour/teambeam/model/dto/member/RegisterDto.java
@@ -1,28 +1,4 @@
 package passionmansour.teambeam.model.dto.member;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class RegisterDto {
-
-    @NotNull(message = "Name cannot be null")
-    private String memberName;
-
-    @NotNull(message = "mail cannot be null")
-    private String mail;
-
-    @NotNull(message = "password cannot be null")
-    private String password;
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String token;
-
 }
