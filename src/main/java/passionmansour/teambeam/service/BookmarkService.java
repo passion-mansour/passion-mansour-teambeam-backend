d91976cfbd3d519cb43b6bca2fcb0ef776984c79
SeoYeon Lee <ysl04030@gmail.com>
2024-05-31 13:13:20 +0900

    Fix : 북마크 전체 조회 수정

--- src/main/java/passionmansour/teambeam/service/BookmarkService.java
+++ src/main/java/passionmansour/teambeam/service/BookmarkService.java
@@ -2,26 +2,20 @@ package passionmansour.teambeam.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import passionmansour.teambeam.model.dto.board.response.BookmarkListResponse;
 import passionmansour.teambeam.model.dto.board.response.BookmarkResponse;
-import passionmansour.teambeam.model.dto.board.response.PostListResponse;
 import passionmansour.teambeam.model.dto.board.response.PostResponse;
 import passionmansour.teambeam.model.entity.Bookmark;
 import passionmansour.teambeam.model.entity.Member;
 import passionmansour.teambeam.model.entity.Post;
 import passionmansour.teambeam.repository.BookmarkRepository;
-import passionmansour.teambeam.repository.PostRepository;
 import passionmansour.teambeam.service.board.PostService;
 import passionmansour.teambeam.service.security.JwtTokenService;
 
-import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -65,27 +59,26 @@ public class BookmarkService {
     }
 
     @Transactional(readOnly = true)
-    public PostListResponse findAllByToken(String token){
+    public BookmarkListResponse findAllByToken(String token){
         Member member = jwtTokenService.getMemberByToken(token);
-        List<PostResponse> postResponses = new ArrayList<>();
+        List<BookmarkResponse> bookmarkResponses = new ArrayList<>();
 
         for(Bookmark bookmark : member.getBookmarks()){
-            postResponses.add(new PostResponse().form(bookmark.getPost()));
+            bookmarkResponses.add(new BookmarkResponse().form(bookmark));
         }
 
-        return new PostListResponse().form(postResponses);
+        return new BookmarkListResponse().form(bookmarkResponses);
     }
 
     @Transactional(readOnly = true)
-    public PostListResponse getAllByTags(String token, List<Long> tagIds){
+    public BookmarkListResponse getAllByTags(String token, List<Long> tagIds){
         Member member = jwtTokenService.getMemberByToken(token);
-
-        List<PostResponse> postResponses = new ArrayList<>();
+        List<BookmarkResponse> bookmarkResponses = new ArrayList<>();
 
         for(Bookmark bookmark : bookmarkRepository.findAllByTagIds(member.getMemberId(), tagIds, tagIds.size())){
-            postResponses.add(new PostResponse().form(bookmark.getPost()));
+            bookmarkResponses.add(new BookmarkResponse().form(bookmark));
         }
 
-        return new PostListResponse().form(postResponses);
+        return new BookmarkListResponse().form(bookmarkResponses);
     }
 }
