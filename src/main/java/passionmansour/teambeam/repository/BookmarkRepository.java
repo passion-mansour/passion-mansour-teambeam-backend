7c9d40b04d0dd21dbabec5c146901fe59e417e04
SeoYeon Lee <ysl04030@gmail.com>
2024-05-31 18:13:58 +0900

    Fix : tag 서비스 수정

--- src/main/java/passionmansour/teambeam/repository/BookmarkRepository.java
+++ src/main/java/passionmansour/teambeam/repository/BookmarkRepository.java
@@ -15,14 +15,12 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     @Query("SELECT b FROM Bookmark b JOIN b.post p JOIN p.postTags t WHERE t.postTagId IN :postTagId AND b.member.memberId IN :memberId")
     List<Bookmark> findAllByTag(@Param("postTagId") Long postTagId, @Param("memberId") Long memberId);
 
-    @Query(value = "SELECT b.* " +
+    @Query(value = "SELECT DISTINCT b.* " +
             "FROM bookmark b " +
             "JOIN post p ON b.post_id = p.post_id " +
             "JOIN post_tag pt ON p.post_id = pt.post_id " +
-            "WHERE b.member_id = :memberId AND pt.tag_id IN (:tagIds) " +
-            "GROUP BY b.bookmark_id, p.post_id " +
-            "HAVING COUNT(DISTINCT pt.tag_Id) = :tagCount",
+            "WHERE b.member_id = :memberId AND pt.tag_id IN (:tagIds)",
             nativeQuery = true)
-    List<Bookmark> findAllByTagIds(@Param("memberId") Long memberId, @Param("tagIds") List<Long> tagIds, @Param("tagCount") int tagCount);
+    List<Bookmark> findAllByTagIds(@Param("memberId") Long memberId, @Param("tagIds") List<Long> tagIds);
 
 }
