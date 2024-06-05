d3c0d1c72598384b48780ca7b5d9bc3728e42e6f
kitewater <97283971+kitewater@users.noreply.github.com>
2024-06-05 15:25:21 +0900

    fix : redis 초기화 기능 수정
    개발중 일관성을 위해 redis를 초기화 하는 기능을 수정했습니다.

--- src/main/java/passionmansour/teambeam/TeambeamApplication.java
+++ src/main/java/passionmansour/teambeam/TeambeamApplication.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.core.RedisTemplate;
 import passionmansour.teambeam.service.redis.RedisService;
 
 import java.time.LocalDateTime;
@@ -15,14 +17,30 @@ import java.time.LocalDateTime;
 public class TeambeamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TeambeamApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(TeambeamApplication.class, args);
 		System.out.println("hello world");
+
+		context.registerShutdownHook();
+
+		// 종료 후크 추가
+		context.addApplicationListener(event -> {
+			if (event instanceof org.springframework.context.event.ContextClosedEvent) {
+				System.out.println("Shutting down...");
+				try {
+					RedisTemplate<String, Object> redisTemplate = context.getBean("redisTemplate", RedisTemplate.class);
+					redisTemplate.getConnectionFactory().getConnection().flushDb();
+					System.out.println("Redis 데이터가 초기화되었습니다.");
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("Redis 데이터 초기화 실패: " + e.getMessage());
+				}
+			}
+		});
 	}
 
 	@Bean
-	public CommandLineRunner run(RedisService redisService, ObjectMapper objectMapper) {
+	public CommandLineRunner run(ObjectMapper objectMapper) {
 		return args -> {
-			redisService.checkRedisConnection();
 			System.out.println(objectMapper.writeValueAsString(LocalDateTime.now()));
 		};
 	}
