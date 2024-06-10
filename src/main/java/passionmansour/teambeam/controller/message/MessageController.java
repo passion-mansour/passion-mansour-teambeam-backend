9e11b4315c1e0ba32abfd18669aef46dad48fc6f
kitewater <97283971+kitewater@users.noreply.github.com>
2024-06-11 02:35:34 +0900

    feat ; coment 업로드 기능 추가

--- src/main/java/passionmansour/teambeam/controller/message/MessageController.java
+++ src/main/java/passionmansour/teambeam/controller/message/MessageController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import passionmansour.teambeam.model.dto.message.MessageCommentDTO;
 import passionmansour.teambeam.model.dto.message.MessageDTO;
+import passionmansour.teambeam.service.message.MessageCommentService;
 import passionmansour.teambeam.service.message.MessageService;
 
 import java.util.List;
@@ -14,14 +16,22 @@ import java.util.List;
 @RequestMapping("/api/team")
 public class MessageController {
     private final MessageService messageService;
+    private final MessageCommentService messageCommentService;
 
     @Autowired
-    public MessageController(MessageService messageService) {
+    public MessageController(MessageService messageService, MessageCommentService messageCommentService) {
         this.messageService = messageService;
+        this.messageCommentService = messageCommentService;
     }
 
     @GetMapping("/chat/{projectId}")
     public List<MessageDTO> getMessages(@PathVariable Long projectId) {
         return messageService.getMessages(projectId);
+    }
+
+    @GetMapping("/chat/{projectId}/{messageId}")
+    public List<MessageCommentDTO> getComment(@PathVariable Long projectId, @PathVariable Long messageId) {
+        List<MessageCommentDTO> messages = messageCommentService.getAllComment(messageId);
+        return messages;
     }
 }
